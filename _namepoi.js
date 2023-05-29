5754c7d9d37d2dd570845a4e4c06495713b12615
darkstack <1835601+darkstack@users.noreply.github.com>
2023-05-29 10:16:37 +0200

    Userlist

--- _namepoi.js
+++ _namepoi.js
@@ -7,41 +7,62 @@
 // ==/UserScript==
 
 document.querySelector('head').appendChild(document.createElement('script').appendChild(document.createTextNode('(' + async function inject2() {
-  
+
+  var vueAppTest = null;
+
+
+  var usersList = function ()
+  {
+    if(!!vueAppTest.users){
+     return Object.entries(vueApp.users).map(([key,value])=>{return {'id' : key,'name' :value.name}});Object.entries(vueApp.users).map(([key,value])=>{return {'id' : key,'name' :value.name}});
+    }
+    else{
+     return vueAppTest.getUserListForListPopup();
+    }
+  }
+
+
   var autoComplete = function (e){
     console.log(this.name);
     if(e.keyCode == 9){
       var input = document.getElementById('input-textbox');
       var text = input.value.split(' ');
       var lastWord = text[text.length - 1];
-      for (const [key, value] of Object.entries(vueApp.users)){
-         console.log(`${key}: ${value.name}`);
-         if(value.name.indexOf(lastWord)== 0){
-           input.value = input.value + value.name.substring(lastWord.length)+' ';
-           e.preventDefault()
-           return;
-         }
-			}
+      for (user of Object.entries(vueApp.users)){
+        if(user.name.indexOf(lastWord)== 0){
+          input.value = input.value + user.name.substring(lastWord.length)+' ';
+          e.preventDefault()
+          return;
+        }
+      }
     }
   }
-  
-  
+
+
   console.log("OK");
-  
+
   var check = function(changes, observer) {
-    	console.log('obs');
-      var el = document.getElementById('input-textbox')
-      if(el) {
-        	console.log("Adios");
-        	el.addEventListener("keydown", (e)=>{autoComplete(e)}, false);
-          observer.disconnect();
+    console.log('obs');
+    var el = document.getElementById('input-textbox')
+    if(el) {
+      console.log("Adios");
+      if(location.host === 'gikopoipoi.net')
+      {
+        vueAppTest = vueApp._contenair._vnode.component.proxy;
       }
+      else
+      {
+        vueAppTest = vueApp;
+      }
+      el.addEventListener("keydown", (e)=>{autoComplete(e)}, false);
+      observer.disconnect();
+    }
   }
-  
+
   var observer = new MutationObserver(check);
-  
+
   observer.observe(document, {childList: true, subtree: true});
-	console.log(observer);
-	
-  
+  console.log(observer);
+
+
 }+')()')).parentNode);
